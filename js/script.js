8c0ae3c5b160ee5f53dfcb286e4b324a525030b4
mathzaday <matheus.h.paula6@aluno.senai.br>
2025-06-24 08:25:17 -0300

    testing

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const numero = parseInt(document.getElementById('numero')).value;
+const enviar = document.getElementById('enviar')
+
+enviar.addEventListener('click', send)
+
+function send(){
+    
+}
