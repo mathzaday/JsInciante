08d4b7731f17da0c993681999675ac3e0d70335a
mathzaday <matheus.h.paula6@aluno.senai.br>
2025-06-24 09:09:55 -0300

    testing

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
-const numero = parseInt(document.getElementById('numero')).value;
-const enviar = document.getElementById('enviar')
-
 enviar.addEventListener('click', send)
 
 function send(){
-    
+    const result = document.getElementById('resul');
+    const num = document.getElementById('numero').value;
+
+    for (let i = 1; i <= 10; i++){
+
+        let resultado = num * i
+
+        const line = document.createElement('p')
+
+        line.textContent = `Número ${num} x ${i} = ${resultado}`
+        
+        result.appendChild(line)
+    }
+    numero.value ()
 }
